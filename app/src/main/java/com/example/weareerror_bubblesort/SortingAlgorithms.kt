c64d372b368ea1449a086307d65ab403bfb90396
corbinabeebe <prplefrutlup@cb.local>
2020-09-10 15:16:54 -0700

    added sorting class and outline of bubble sort function

---
+++ app/src/main/java/com/example/weareerror_bubblesort/SortingAlgorithms.kt
@@ -0,0 +1,11 @@
+package com.example.weareerror_bubblesort
+
+class SortingAlgorithms {
+
+    fun BubbleSort(userArray: IntArray) {
+
+        //first for loop that loops through the user array
+
+        //second for loop for bubble sort
+    }
+}
