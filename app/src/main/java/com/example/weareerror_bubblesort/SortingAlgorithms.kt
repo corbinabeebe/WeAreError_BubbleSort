a4c7540518394b9b33020b6b3502d5ec9686c19a
dvenardos <dvenardos@gmail.com>
2020-09-13 11:01:20 -0700

    Add sorting algorithm.

--- app/src/main/java/com/example/weareerror_bubblesort/SortingAlgorithms.kt
+++ app/src/main/java/com/example/weareerror_bubblesort/SortingAlgorithms.kt
@@ -3,9 +3,29 @@ package com.example.weareerror_bubblesort
 class SortingAlgorithms {
 
     fun BubbleSort(userArray: IntArray) {
+        var swapCount:Int
 
-        //first for loop that loops through the user array
-
-        //second for loop for bubble sort
+        do {
+            swapCount = SortArray(userArray)
+        }while (swapCount > 0)
     }
+
+    fun SortArray(sortArray: IntArray):Int{
+        var swapCount = 0
+
+        for (i in (sortArray.size - 1) downTo 1){
+            if (sortArray[i] < sortArray[i - 1] ){
+                val lowerValue = sortArray[i]
+                val higherValue = sortArray[i - 1]
+
+                sortArray[i] = higherValue
+                sortArray[i - 1] = lowerValue
+
+                swapCount++
+            }
+        }
+
+        return swapCount
+    }
+
 }
