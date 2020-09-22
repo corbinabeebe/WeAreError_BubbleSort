0fa5355f6c857d2447cd4a37ef314bdbf69a247f
Corbin Beebe <corbinbeebe@gmail.com>
2020-09-22 13:08:11 -0700

    added SortActivity and removed un-needed fragments

---
+++ app/src/main/java/com/example/weareerror_bubblesort/SortActivity.kt
@@ -0,0 +1,11 @@
+package com.example.weareerror_bubblesort
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SortActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sort)
+    }
+}
