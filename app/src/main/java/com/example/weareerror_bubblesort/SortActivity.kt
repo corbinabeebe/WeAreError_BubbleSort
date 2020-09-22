b471d663c5516b91251d4bb77133fd2b7b4fd6ac
Corbin Beebe <corbinbeebe@gmail.com>
2020-09-22 13:26:08 -0700

    added onlick listener to WelcomeActivity and Sort Activity

--- app/src/main/java/com/example/weareerror_bubblesort/SortActivity.kt
+++ app/src/main/java/com/example/weareerror_bubblesort/SortActivity.kt
@@ -2,10 +2,15 @@ package com.example.weareerror_bubblesort
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sort.*
 
 class SortActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sort)
+
+        sortArrayButton.setOnClickListener {
+
+        }
     }
 }
--- app/src/main/java/com/example/weareerror_bubblesort/WelcomeActivity.kt
+++ app/src/main/java/com/example/weareerror_bubblesort/WelcomeActivity.kt
@@ -1,15 +1,12 @@
 package com.example.weareerror_bubblesort
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 //var userEntryTextView
 //var sortedTextView
-//var sortButton
-
 //var userArray: IntArray
 
 class WelcomeActivity : AppCompatActivity() {
@@ -18,8 +15,10 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-
-
+        getStartedButton.setOnClickListener {
+            val sortIntent = Intent(this, SortActivity:: class.java)
+            startActivity(sortIntent)
+        }
     }
 
 }
