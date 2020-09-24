e7c1fa699df5247ed214607c3af9380dcb88ec62
Corbin Beebe <corbinbeebe@gmail.com>
2020-09-23 20:00:13 -0700

    added userinput subscript errors concatination

--- app/src/main/java/com/example/weareerror_bubblesort/SortActivity.kt
+++ app/src/main/java/com/example/weareerror_bubblesort/SortActivity.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_sort.*
 //used to validate that all digits entered are numbers
 val validNumbers = setOf("0","1","2","3","4","5","6","7","8","9")
 
+//function used in this context to create a toast notification
 fun Context.toast(message: CharSequence) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 
 class SortActivity : BaseActivity() {
@@ -39,7 +40,7 @@ class SortActivity : BaseActivity() {
 
             for(i in 0 until inputLength!!.minus(1)) {
                 if (!validNumbers.contains(userInput[i].toString())) {
-                    errors += i
+                    errors += userInput[i].toString()
                 }
             }
 
