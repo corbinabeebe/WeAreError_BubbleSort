dea9e1d3593649c5a5a30a2d017b1d88396e1ddc
dvenardos <dvenardos@gmail.com>
2020-09-29 17:34:37 -0700

    Renamed single pass sort function.

--- app/src/test/java/com/example/weareerror_bubblesort/SortingAlgorithmsTest.kt
+++ app/src/test/java/com/example/weareerror_bubblesort/SortingAlgorithmsTest.kt
@@ -4,15 +4,40 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-val sort = BubbleSort()
+val bubbleSort = BubbleSort()
 
 class SortingAlgorithmsTest {
     @Test
+    fun validate_bubble_sort (){
+        var inputValues = intArrayOf(9, 8, 3, 2, 4, 6)
+        var expectedResult = "9 8 3 2 4 6\n" +
+                "2 9 8 3 4 6\n" +
+                "2 3 9 8 4 6\n" +
+                "2 3 4 9 8 6\n" +
+                "2 3 4 6 9 8\n" +
+                "2 3 4 6 8 9"
+        bubble_sort_valid(inputValues, expectedResult)
 
-    fun bubble_sort_valid() {
-        assertEquals(arrayListOf(2, 1, 5, 6), sort.bubbleSort(intArrayOf(2, 5, 2, 1, 6)))
+        inputValues = intArrayOf(1, 2, 3, 4, 5)
+        expectedResult = "1 2 3 4 5"
+        bubble_sort_valid(inputValues, expectedResult)
+
+        inputValues = intArrayOf(7, 9, 8, 1, 0)
+        expectedResult = "7 9 8 1 0\n" +
+                "0 7 9 8 1\n" +
+                "0 1 7 9 8\n" +
+                "0 1 7 8 9"
+        bubble_sort_valid(inputValues, expectedResult)
+
     }
 
+    private fun bubble_sort_valid(inputArray: IntArray, expectedIterations :String) {
 
+        var iterations = bubbleSort.sort(inputArray)
+        println(iterations)
+        println()
+
+        assertEquals(expectedIterations, iterations)
+    }
 
 }
